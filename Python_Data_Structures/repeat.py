7fc7b5ee66ae14d5d1d0c6356ef14e5a1d37e916
shane <shanesilvernail3@gmail.com>
2021-09-02 20:11:23 -0400

    finished more exercises

---
+++ Python_Data_Structures/repeat.py
@@ -0,0 +1,27 @@
+def repeat(phrase, num):
+    """Return phrase, repeated num times.
+
+        >>> repeat('*', 3)
+        '***'
+
+        >>> repeat('abc', 2)
+        'abcabc'
+
+        >>> repeat('abc', 0)
+        ''
+
+    Ignore illegal values of num and return None:
+
+        >>> repeat('abc', -1) is None
+        True
+
+        >>> repeat('abc', 'nope') is None
+        True
+    """
+    # if num >= 0 or isinstance(num, int):
+    #     return phrase * num
+    # return None
+
+    if not isinstance(num, int) or num < 0:
+        return None
+    return phrase * num
---
+++ Python_Data_Structures/same_frequency.py
@@ -0,0 +1,15 @@
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+    
+        >>> same_frequency(551122, 221515)
+        True
+        
+        >>> same_frequency(321142, 3212215)
+        False
+        
+        >>> same_frequency(1212, 2211)
+        True
+    """
+    if set(num1) == set(num2):
+        return True
+    return False
