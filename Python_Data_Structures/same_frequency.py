ecc7a8f858e845afdc9bf1f0ebe9cf6a5d2ff470
shane <shanesilvernail3@gmail.com>
2021-09-05 18:13:45 -0400

    finished python exercises

--- Python_Data_Structures/same_frequency.py
+++ Python_Data_Structures/same_frequency.py
@@ -1,15 +1,24 @@
+def freq_counter(coll):
+
+    counts = {}
+
+    for x in coll:
+        counts[x] = counts.get(x, 0) + 1
+
+    return counts
+
+
 def same_frequency(num1, num2):
     """Do these nums have same frequencies of digits?
-    
+
         >>> same_frequency(551122, 221515)
         True
-        
+
         >>> same_frequency(321142, 3212215)
         False
-        
+
         >>> same_frequency(1212, 2211)
         True
     """
-    if set(num1) == set(num2):
-        return True
-    return False
+
+    return freq_counter(str(num1)) == freq_counter(str(num2))
