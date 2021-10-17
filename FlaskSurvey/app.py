e9db1329cd8e239c704a5c803ff976774a682e78
shane <shanesilvernail3@gmail.com>
2021-10-17 17:40:40 -0400

    used flask sessions

--- FlaskSurvey/app.py
+++ FlaskSurvey/app.py
@@ -1,6 +1,7 @@
 from logging import debug
-from flask import Flask, render_template, request, redirect, flash
-from flask_debugtoolbar import DebugToolbarExtension 
+from flask import Flask, render_template, request, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.wrappers import response 
 from surveys import Question, satisfaction_survey as survey 
 
 
@@ -11,23 +12,25 @@ app.config['SECRET_KEY'] = 'shsboi33'
 debug = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-RESPONSES = []
+RESPONSES_KEY = 'responses' 
 
 
 
 @app.route('/')
 def start_survey():
+
     return render_template('home.html')
 
-
-# @app.route('/questions/0')
-# def first_question():
-#     return render_template('question_one.html')
+@app.route('/session-answer', methods=['POST'])
+def start_surveys():
+        session[RESPONSES_KEY] = []
+        return redirect('/questions/0')
 
 
 @app.route('/questions/<int:qid>')
 def second_question(qid):
-    responses = RESPONSES
+    responses = session.get(RESPONSES_KEY)
+
     if (len(responses) != qid):
         flash(f"Invalid question id: {qid}.")
         return redirect(f"/questions/{len(responses)}")
@@ -39,8 +42,9 @@ def second_question(qid):
 @app.route('/answer', methods=['POST'])
 def add_answer():
     answer = request.form['answer']
-    RESPONSES.append(answer)
-    responses = RESPONSES
+    responses = session[RESPONSES_KEY]
+    responses.append(answer)
+    session[RESPONSES_KEY] = responses
     
     if (len(responses) == len(survey.questions)):
         # They've answered all the questions! Thank them.
